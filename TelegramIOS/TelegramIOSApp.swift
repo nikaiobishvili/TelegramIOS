ffca531478bb927c02ee069e795d7d39122eb6da
Nika Iobishvili <nika.iobishvili@gmail.com>
2025-01-27 15:29:44 +0400

    add firebase sdk

--- TelegramIOS/TelegramIOSApp.swift
+++ TelegramIOS/TelegramIOSApp.swift
@@ -1,8 +1,14 @@
 
 import SwiftUI
+import Firebase
 
 @main
 struct TelegramIOSApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
