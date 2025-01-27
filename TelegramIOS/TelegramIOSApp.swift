c06b512b91c14eef47b257f4a1effcfd400fac4d
Nika Iobishvili <nika.iobishvili@gmail.com>
2025-01-27 12:07:13 +0400

    Initial Commit

---
+++ TelegramIOS/TelegramIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramIOSApp.swift
+//  TelegramIOS
+//
+//  Created by Nika on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct TelegramIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TelegramIOSTests/TelegramIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TelegramIOSTests.swift
+//  TelegramIOSTests
+//
+//  Created by Nika on 1/27/25.
+//
+
+import Testing
+@testable import TelegramIOS
+
+struct TelegramIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
